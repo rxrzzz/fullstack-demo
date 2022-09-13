e88bf152af77fe3f5ef7c0c77f3930545e6e4ec1
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-09-13 06:29:18 +0100

    setting up server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 
 //express app
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.json({mssg: "Welcome to the app"})
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
 
 // listen for requests
-app.listen(4000, () => {
-    console.log("listening on port 4000")
-})
+app.listen(process.env.PORT, () => {
+  console.log("listening on port 4000");
+});
