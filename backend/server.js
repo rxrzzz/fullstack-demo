8849d63b8a2dfab5339c8302546e48c56469041c
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-09-14 09:33:01 +0100

    setting up mongo and workout model

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 require("dotenv").config();
 const express = require("express");
-
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
 //express app
 const app = express();
 
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port 4000");
+    });
+  })
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
-
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app" });
+  console.log(req.path, req.method);
+  next();
 });
 
+app.use("/api/workouts", workoutRoutes);
 // listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000");
-});
