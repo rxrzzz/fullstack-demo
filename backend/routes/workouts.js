d46b70a1887a8650491f88b1b72e07a268de2459
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-09-14 10:52:24 +0100

    adding test data to mongo

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,28 +1,44 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const Workout = require("../models/workoutModel");
+const router = express.Router();
 
 //get all workouts
-router.get('/', (req, res) => {
-    res.json({msg: "GET all workouts"})
-})
+router.get("/", (req, res) => {
+  res.json({ msg: "GET all workouts" });
+});
 
 //post a new workout
-router.get('/:id', (req, res) => {
-    res.json({msg: "Get single workout."})
-})
+router.get("/:id", (req, res) => {
+  res.json({ msg: "Get single workout." });
+});
 
 //post a new workout
-router.post('/', (req, res) => {
-    res.json({msg: "Post a new workout"})
-})
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (err) {
+    res.json({ msg: "Error" });
+  }
+});
 
-router.delete('/:id', (req, res) => {
-    res.json({msg: "Delete a workout"})
-})
+//for (backend testing 😶)
+// router.post("/", async (req, res) => {
+//     const { title, load, reps } = req.body;
+//     try {
+//       const workout = await Workout.create({ title, load, reps });
+//       res.status(200).json(workout);
+//     } catch (err) {
+//       res.json({ msg: "Error" });
+//     }
+//   });
+router.delete("/:id", (req, res) => {
+  res.json({ msg: "Delete a workout" });
+});
 
-router.patch('/:id', (req, res) => {
-    res.json({msg: "Update a workout"})
-})
+router.patch("/:id", (req, res) => {
+  res.json({ msg: "Update a workout" });
+});
 
-
-module.exports = router
+module.exports = router;
