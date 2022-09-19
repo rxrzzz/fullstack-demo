eecf9626e6d035845a57e75b76228f5f6a046fb3
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-09-19 14:05:10 +0100

    hashed passwords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+c
 
 const Schema = mongoose.Schema;
 
@@ -16,5 +18,19 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+//static signup method
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
