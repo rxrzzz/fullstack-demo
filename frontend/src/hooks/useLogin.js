8846c77a998c42ef148e2571a02b24e9887798cb
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-09-20 10:40:12 +0100

    created usesignup hook

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,6 +1,33 @@
 import { useState } from "react";
+import { useAuthContext } from "../context/authContext";
 
 export const useLogin = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setisLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const signup = async (email, password) => {
+    setisLoading(true);
+    setError(null);
+
+    const response = await fetch("/api/user/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = await response.json();
+    if (!response.ok) {
+      setisLoading(false);
+      setError(json.error);
+    }
+    if (response.ok) {
+      //save the user to local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      //update the auth context
+      dispatch({ type: "LOGIN", payload: json });
+    }
+  };
+
+  return { signup, isLoading, error };
 };
